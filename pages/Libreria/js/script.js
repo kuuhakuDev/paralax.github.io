c2809e63e821d58b62d53a61807934c29f561a33
kuuhakuDev <kuuhakuDeveloper@outlook.com>
2020-01-08 22:26:36 -0600

    Agregada pagina de LibreriaSoft

---
+++ pages/Libreria/js/script.js
@@ -0,0 +1,6 @@
+$(window).resize(function(){
+    //aqui el codigo que se ejecutara cuando se redimencione la ventana
+    var alto=$(window).height();
+    var ancho=$(window).width();
+    alert("alto: "+alto+" ancho:"+ancho);
+    });
